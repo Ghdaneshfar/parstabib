ad04b83ba5c1437b00b3126e901b0b46e91b53b4
Ghdaneshfar <lorgil1@yahoo.com>
2020-06-20 18:45:35 +0430

    changes gas done

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
 require('dns').lookup(require('os').hostname(), function (err, add, fam) {
     console.log('addr: '+add);
-  })
-  
-app.get('/', (req, resp) => {
+  });
+
+
+  app.get('/', (req, resp) => {
     resp.send('hello dears');
 });
 app.listen(port, () => {
